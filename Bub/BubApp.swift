4249c17eb7badc6a86d33cea763e3c75847d3287
amisra <amisra@expediagroup.com>
2025-07-03 20:04:20 +0530

    Initial Commit

---
+++ Bub/BubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubApp.swift
+//  Bub
+//
+//  Created by Abhinav Misra on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubTests/BubTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubTests.swift
+//  BubTests
+//
+//  Created by Abhinav Misra on 03/07/25.
+//
+
+import Testing
+@testable import Bub
+
+struct BubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
